ca1f254a74277b106818f9cc27a9008c984b3d1e
Osvaldo Ortega <48293249+osortega@users.noreply.github.com>
2023-03-03 23:37:26 +0000

    -n

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo $GH_CLI_TOKEN >> ~/.copilot-cli-access-token
-echo $GH_COPILOT_CLI_TOKEN >> ~/.copilot-cli-copilot-token
+echo -n $GH_CLI_TOKEN >> ~/.copilot-cli-access-token
+echo -n $GH_COPILOT_CLI_TOKEN >> ~/.copilot-cli-copilot-token
 
 npm install -g @githubnext/github-copilot-cli
 eval "$(github-copilot-cli alias -- "$0")"
